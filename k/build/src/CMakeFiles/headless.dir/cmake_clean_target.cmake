c400fcdff6b71e15c3b51ec075ec37646da8d557
AmyYang96 <hpfanamy@gmail.com>
2017-09-26 11:05:18 -0700

    added kilombo

---
+++ k/build/src/CMakeFiles/headless.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libheadless.a"
+)
---
+++ k/build/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/amy/Downloads/kilombo/src
+# Build directory: /home/amy/Downloads/kilombo/build/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
